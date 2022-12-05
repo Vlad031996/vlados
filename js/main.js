5d3d46d6ad9b045524fac795e43101a3503b2dc5
Vlad031996 <111621998+Vlad031996@users.noreply.github.com>
2022-12-05 22:52:15 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(function () {
+
+  $(".rateYo").rateYo({
+    starWidth: "36px"
+  });
+ 
+
+  var mixer = mixitup('.slider__transition');
+
+
+
+ $('.clients__slider').slick({
+  arrows: false,
+  infinite: true,
+  waitForAnimate: false,
+  dots: true,
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
