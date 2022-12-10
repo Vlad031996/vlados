f933c5d52520a68e1d2c0a2fdf9443bb564a7e42
Vlad031996 <111621998+Vlad031996@users.noreply.github.com>
2022-12-10 21:44:28 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(function () {
+
+  $('.rateYo').rateYo({
+    starWidth: "36px"
+  });
+ 
+
+  var mixer = mixitup('.slider__transition');
+  
+  // ================Slider=========================
+  $('.clients__slider').slick({
+    arrows: false,
+    slidesToShow: 2,
+    dots: true,
+    slidesToScroll: 1,
+ })
+ // ======================Slider-arrow===================
+ 
+ // ======================Slider-arrow===================
+ $('.clients__slide-prev').on('click', function (e) {
+  e.preventDefault()
+  $('.clients__slider').slick('slickPrev')
+})
+$('.clients__slide-next').on('click', function (e) {
+  e.preventDefault()
+  $('.clients__slider').slick('slickNext')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
